eba9c4a700c2d92fc933fbf3f00611e9f3e9caf1
Joe Pea <joe@trusktr.io>
2023-01-13 10:50:34 -0800

    get the birds demo working

--- demo/js/Bird.js
+++ demo/js/Bird.js
@@ -1,40 +1,23 @@
-var Bird = function () {
+import * as THREE from 'three'
 
-	var scope = this;
+export class Bird extends THREE.BufferGeometry {
+	constructor() {
+		super()
 
-	THREE.Geometry.call( this );
+		// prettier-ignore
+		const points = [
+            5, 0, 0,
+            -5, -2, 1,
+            -5, 0, 0,
+            -5, -2, -1,
 
-	v(   5,   0,   0 );
-	v( - 5, - 2,   1 );
-	v( - 5,   0,   0 );
-	v( - 5, - 2, - 1 );
+            0, 2, -6,
+            0, 2, 6,
+            2, 0, 0,
+            -3, 0, 0,
+        ]
 
-	v(   0,   2, - 6 );
-	v(   0,   2,   6 );
-	v(   2,   0,   0 );
-	v( - 3,   0,   0 );
-
-	f3( 0, 2, 1 );
-	// f3( 0, 3, 2 );
-
-	f3( 4, 7, 6 );
-	f3( 5, 6, 7 );
-
-	this.computeFaceNormals();
-
-	function v( x, y, z ) {
-
-		scope.vertices.push( new THREE.Vector3( x, y, z ) );
-
+		const positions = new THREE.Float32BufferAttribute(Float32Array.from(points), 3)
+		this.setAttribute('position', positions)
 	}
-
-	function f3( a, b, c ) {
-
-		scope.faces.push( new THREE.Face3( a, b, c ) );
-
-	}
-
 }
-
-Bird.prototype = Object.create( THREE.Geometry.prototype );
-Bird.prototype.constructor = Bird;
